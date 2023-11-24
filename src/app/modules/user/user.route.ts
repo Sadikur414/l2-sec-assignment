77f92f8aab21b1fddf3a8e425b23dfce342bac1b
SADIKUR RAHMAN SAGOR <sadikursagor414@gmail.com>
2023-11-24 16:31:18 +0600

    application router are setup

--- src/app/modules/user/user.route.ts
+++ src/app/modules/user/user.route.ts
@@ -5,9 +5,9 @@ import { UserController } from './user.controller'
 const router = express.Router()
 
 
-router.post("/", UserController.createUser);
-router.get('/', UserController.getAllUsers);
-router.get('/:userId', UserController.getSingleUser)
+router.post("/api/users", UserController.createUser);
+router.get('/api/users', UserController.getAllUsers);
+router.get('/api/users/:userId', UserController.getSingleUser)
 
 
 export const UserRoutes = router;  
